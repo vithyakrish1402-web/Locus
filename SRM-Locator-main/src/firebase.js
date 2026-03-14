2d842552ae1618e0d7e2df6ced38d30072be7b52
Vithya Krishnan R A <vithyakrish1402@gmail.com>
2026-03-14 23:50:03 +0530

    Integrated Google Auth and dynamic map markers

---
+++ SRM-Locator-main/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLHdYE-DooW-7eZ8HGsJv01CSXW5hnfeY",
+  authDomain: "locus-5c6a8.firebaseapp.com",
+  projectId: "locus-5c6a8",
+  storageBucket: "locus-5c6a8.firebasestorage.app",
+  messagingSenderId: "146325179738",
+  appId: "1:146325179738:web:1f0f429750b4a662057763"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
